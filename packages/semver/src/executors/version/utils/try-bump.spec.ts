84ef26c6ac66eb30b810e2d9b21e4c3087e3093c
hoonoh <hoonoh@users.noreply.github.com>
2022-01-01 18:34:22 +0900

    fix: handle multiple deps updates

--- packages/semver/src/executors/version/utils/try-bump.spec.ts
+++ packages/semver/src/executors/version/utils/try-bump.spec.ts
@@ -107,11 +107,11 @@ describe('tryBump', () => {
           { name: 'dep2', path: '/libs/dep2' },
         ],
         tagPrefix: 'v',
+        syncVersions: true,
       })
     );
 
-    // ? should this be `2.1.1` ? #278
-    expect(newVersion?.version).toEqual('2.2.0');
+    expect(newVersion?.version).toEqual('2.1.1');
 
     expect(mockGetCommits).toBeCalledTimes(3);
     expect(mockGetCommits).toBeCalledWith({
@@ -199,6 +199,13 @@ describe('tryBump', () => {
     mockGetLastVersion.mockReturnValue(throwError(() => 'No version found'));
     mockGetCommits.mockReturnValue(of([]));
     mockGetFirstCommitRef.mockReturnValue(of('sha1'));
+    mockConventionalRecommendedBump.mockImplementation(
+      callbackify(
+        jest.fn().mockResolvedValue({
+          releaseType: undefined,
+        })
+      ) as () => void
+    );
 
     await lastValueFrom(
       tryBump({
@@ -220,6 +227,13 @@ describe('tryBump', () => {
 
   it('should return undefined if there are no changes in current path', async () => {
     mockGetCommits.mockReturnValue(of([]));
+    mockConventionalRecommendedBump.mockImplementation(
+      callbackify(
+        jest.fn().mockResolvedValue({
+          releaseType: undefined,
+        })
+      ) as () => void
+    );
 
     const newVersion = await lastValueFrom(
       tryBump({
